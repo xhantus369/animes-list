8ed4521945417ae8cc47227b087280cefa12a8d8
xhantus369 <maikoncandido00@gmail.com>
2024-02-29 19:23:34 -0300

    Primeiros arquivos

---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const router = require("./router");
+
+
+app.use(router);
+
+
+module.exports = app;
---
+++ backend/src/server.js
@@ -0,0 +1,5 @@
+const app = require("./app");
+
+app.listen(8080, () =>{
+    console.log("server starting in port 8080");
+});
